dee52d0b639f9a257feb1c8b8f4ec851e0d765fe
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-09 21:53:32 -0400

    brought over my elements from my html file into my js file

---
+++ script.js
@@ -0,0 +1,9 @@
+const gridContainer = document.querySelector(".grid-container")
+const resetButton = document.querySelector(".reset-button")
+const blackButton = document.querySelector(".black-button")
+const rainbowButton = document.querySelector(".rainbow-button")
+const eraseButton = document.querySelector(".erase-button")
+const gridSizeButton = document.querySelector(".grid-size-button")
+const gridSizeDisplay = document.querySelector(".grid-size-display")
+const currentModeDisplay = document.querySelector(".grid-current-mode-display")
+
